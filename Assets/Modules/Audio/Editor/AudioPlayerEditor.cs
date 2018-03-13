4e20abde02287534fa3ee59d29f51573d8735a8e
TasiYokan <guanghao.yang@outlook.com>
2018-03-13 17:23:03 -0400

    Reorganize folder structure prepare for editor scripts

---
+++ Assets/Modules/Audio/Editor/AudioPlayerEditor.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TasiYokan.Audio;
+using UnityEditor;
+using UnityEngine;
+
+namespace TasiYokan.Audio
+{
+    [CustomEditor(typeof(AudioPlayer))]
+    public class AudioPlayerEditor : Editor
+    {
+        private AudioPlayer m_target;
+        //Serialized Properties
+        private SerializedObject serializedObjectTarget;
+
+        public AudioPlayer Target
+        {
+            get
+            {
+                if (m_target == null)
+                    m_target = (AudioPlayer)target;
+                return m_target;
+            }
+        }
+
+        void OnEnable()
+        {
+            EditorApplication.update += Update;
+
+            if (Target == null) return;
+
+            SetupEditorVariables();
+            GetVariableProperties();
+        }
+
+        void OnDisable()
+        {
+            EditorApplication.update -= Update;
+        }
+
+        void SetupEditorVariables()
+        {
+        }
+
+        void GetVariableProperties()
+        {
+            serializedObjectTarget = new SerializedObject(Target);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Debug.Log("Self update");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObjectTarget.Update();
+            GUILayout.Space(5);
+            GUILayout.Box("Test Text", GUILayout.Width(Screen.width - 20), GUILayout.Height(20));
+            GUILayout.Space(5);
+            serializedObjectTarget.ApplyModifiedProperties();
+        }
+
+        void OnSceneGUI()
+        {
+        }
+    }
+}
